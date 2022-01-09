7c03ee4fe86e27be2c56d29d4a0174820855850c
biswarup roy <69243820+biswarup55655@users.noreply.github.com>
2022-01-09 16:07:16 +0000

    bugs modified

--- Home/lab_assignment/lab_ass5/palinWord.sh
+++ Home/lab_assignment/lab_ass5/palinWord.sh
@@ -3,25 +3,24 @@ palindrome()
 	str=$1
 	flg=0
 	len=`echo $str|wc -c`
-	len=`expr $len -1`
+	len=`expr $len - 1`
 	l=$len
 	for((i=1;i<=len/2;i++))
-		do
-			a=`echo $str|cut -c $i`
-			b=`echo $str|cut -c $l`
-      echo $a
-      echo $b
-			if [ $a != $b ]
-			then
-				flg=1
-				break
-			fi
-			l=`expr $l -1`
-		done
-			if [ $flg -ne 1 ]
-				then
-					echo $str>>palin.txt
-			fi
+        do
+                a=`echo $str|cut -c $i`
+                b=`echo $str|cut -c $l`
+                if [ $a != $b ]
+                then            
+                    flg=1
+                    break
+                fi
+                l=`expr $l - 1`
+	  done
+				if [ $flg -ne 1 ]
+     		 	then
+               	 echo $str>>palin.txt 
+				fi
+        
 }
 rm palin.txt
 for word in $(<a.txt)
@@ -31,7 +30,7 @@ do
 		then
 			count=`wc -c <t.txt`
 			count=$(($count - 2))
-	word=`cut -c 1-$count t.txt`
+	word=`cut -c 1-$count t.txt` 
 	fi
 	palindrome $word
 done
